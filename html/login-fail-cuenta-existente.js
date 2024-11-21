448f1b35924c24a136574f671fed308f3c62f9d9
Brazamm <brachadiazz@gmail.com>
2024-11-21 15:01:52 -0500

    NEW

--- html/login-fail-cuenta-existente.js
+++
@@ -1,7 +0,0 @@
-const signUpButton=document.getElementById('signUpButton');
-const signUpForm=document.getElementById('signUp');
-
-signUpButton.addEventListener('click', function(){
-    signInForm.style.display="block";
-    signUpForm.style.display="none";
-})
